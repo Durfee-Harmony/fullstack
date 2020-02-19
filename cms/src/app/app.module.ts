cf0b905589d98eebd7a314ac9fcd59d1a1c676db
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-19 15:45:40 -0700

    update

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
 
--- cms/src/app/contacts/contacts-detail/contacts-detail.component.ts
+++ cms/src/app/contacts/contacts-detail/contacts-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from '../contacts.model';
+
 @Component({
   selector: 'cms-contacts-detail',
   templateUrl: './contacts-detail.component.html',
   styleUrls: ['./contacts-detail.component.css']
 })
 export class ContactsDetailComponent implements OnInit {
+  contacts: Contact[] = [];
 
   constructor() { }
 
